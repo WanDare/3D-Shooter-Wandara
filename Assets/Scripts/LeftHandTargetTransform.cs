87db4057c2ff78b2e594d157c763db49cd20c234
Neth Wandara <ytdare51@gmail.com>
2025-03-17 09:16:16 +0700

    Initial commit

---
+++ Assets/Scripts/LeftHandTargetTransform.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftHandTargetTransform : MonoBehaviour
+{
+   // we need this script to detech game object
+   // we detecting lefft hand targets position and rotation
+}
---
+++ Assets/Scripts/PlayerAnimationEvent.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationEvent : MonoBehaviour
+{
+    private PlayerWeaponVisuals visualController;
+
+    private void Start()
+    {
+        visualController = GetComponentInParent<PlayerWeaponVisuals>();
+    }
+
+
+    public void ReloadIsOver()
+    {
+        visualController.MaximizeRigWeight();
+    }
+
+
+    public void ReturnRig()
+    {
+        visualController.MaximizeRigWeight();
+        visualController.MaximizeLeftHandWeight();
+    }
+    public void WeaponGrabIsOver()
+    {
+        visualController.SetBusyGrabbingWeaponTo(false);
+    }
+}
